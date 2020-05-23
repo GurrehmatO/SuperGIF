205d046d4fcb3cc29f60aba550d2ee78a2602cba
Gurrehmat Oberoi <gurrehmato@gmail.com>
2020-05-23 19:08:11 +0530

    Updates

---
+++ precache-manifest.9d9065211f5c238391b53c8215f12910.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b026b973b920bb8f67967d6b2c0596f",
+    "url": "/SuperGIF/index.html"
+  },
+  {
+    "revision": "93ad6cba20d3361e3729",
+    "url": "/SuperGIF/static/css/main.c053883b.chunk.css"
+  },
+  {
+    "revision": "92eb56dd845c39e48e16",
+    "url": "/SuperGIF/static/js/2.b07ab89f.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/SuperGIF/static/js/2.b07ab89f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "93ad6cba20d3361e3729",
+    "url": "/SuperGIF/static/js/main.017e2466.chunk.js"
+  },
+  {
+    "revision": "5af8c928a803b0aed93a",
+    "url": "/SuperGIF/static/js/runtime-main.9c8ea512.js"
+  }
+]);
